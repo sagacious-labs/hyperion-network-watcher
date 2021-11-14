2d303719f93b718c9ad8fdd516cd5928f2a07c27
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2021-11-14 17:54:59 +0530

    add entrypoint to the watcher
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

---
+++ cmd/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+
+	"github.com/sagacious-labs/hyperion-network-watcher/pkg/ebpf"
+	"github.com/sagacious-labs/hyperion-network-watcher/pkg/log"
+)
+
+func main() {
+	prog := ebpf.NewTCPConnLat()
+	ch := prog.Start()
+	for data := range ch {
+		log.DataLog(data)
+	}
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+
+	<-stop
+	prog.Stop()
+}
